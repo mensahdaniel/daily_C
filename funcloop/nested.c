5f16c1e764b0823e57a680d1ff0ad72be287df36
mensahdaniel <mensahdaniel5858910@gmail.com>
2023-10-22 22:57:11 +0100

    using printf to print out words

---
+++ funcloop/nested.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int k = 0, j;
+    
+    while (k < 5) {
+        printf("\n");
+        k++;
+    }
+	while (j < 2)
+	{
+		printf ("1 ");
+			j++;
+	}
+    return 0;
+}
